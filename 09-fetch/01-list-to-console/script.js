6e2e7068f4f0b35ed7c2d06d54ea63c1e1f644be
joostvannieu <joost.vannieuwenhuyse@gmail.com>
2019-08-29 16:57:12 +0200

    commit small change to 7 and first 9

--- 09-fetch/01-list-to-console/script.js
+++ 09-fetch/01-list-to-console/script.js
@@ -10,5 +10,30 @@
 // You will have time to focus on it later.
 
 (() => {
+    const fetchString = "http://localhost:63342/js-exercises/_shared/api.json";
     // your code here
+    document.getElementById("run").addEventListener("click", function () {
+    fetch(fetchString)
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error("HTTP error, status = " + response.status);
+            }
+            return response.json();
+        })
+        .then(function (json) {
+            for(var i = 0; i < json.heroes.length; i++) {
+                console.log(json.heroes[i].id);
+                console.log(json.heroes[i].name);
+                console.log(json.heroes[i].alterEgo);
+                console.log(json.heroes[i].abilities);
+            }
+        })
+        .catch(function(error) {
+            var p = document.createElement('p');
+            p.appendChild(
+                document.createTextNode('Error: ' + error.message)
+            );
+            document.body.insertBefore(p, myList);
+        });
+    })
 })();
